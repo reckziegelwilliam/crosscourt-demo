0906c8ff406d83ad54bea53b02b4fb13578a30fd
Liam <reckziegel.william@gmail.com>
2024-06-10 12:43:42 -0700

    temp remove user and tournament api

--- app/api/tournament.ts
+++ app/api/tournament.ts
@@ -1,15 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { db } from '@/lib/db'
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import { db } from '@/lib/db'
 
-// POST /api/tournament
-export default async function handle(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  const result = await db.tournament.create({
-    data: {
-      ...req.body,
-    },
-  })
-  return res.status(201).json(result)
-}
+// // POST /api/tournament
+// export default async function handle(
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+// ) {
+//   const result = await db.tournament.create({
+//     data: {
+//       ...req.body,
+//     },
+//   })
+//   return res.status(201).json(result)
+// }
